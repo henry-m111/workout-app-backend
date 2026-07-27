1b37dc747f4ed4ad8f5ffd858e9cc9b3a00831e0
henry-m111 <henry.muchiri@student.moringaschool.com>
2026-07-27 10:49:31 +0300

    Define Exercise, Workout, and WorkoutExercise models

---
+++ server/models.py
@@ -0,0 +1,43 @@
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.orm import validates
+from datetime import date
+
+db = SQLAlchemy()
+
+
+class Exercise(db.Model):
+    __tablename__ = 'exercises'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String, nullable=False)
+    category = db.Column(db.String, nullable=False)
+    equipment_needed = db.Column(db.Boolean, default=False)
+
+    def __repr__(self):
+        return f'<Exercise {self.id}: {self.name}>'
+
+
+class Workout(db.Model):
+    __tablename__ = 'workouts'
+
+    id = db.Column(db.Integer, primary_key=True)
+    date = db.Column(db.Date, default=date.today)
+    duration_minutes = db.Column(db.Integer, nullable=False)
+    notes = db.Column(db.Text)
+
+    def __repr__(self):
+        return f'<Workout {self.id}: {self.date}>'
+
+
+class WorkoutExercise(db.Model):
+    __tablename__ = 'workout_exercises'
+
+    id = db.Column(db.Integer, primary_key=True)
+    workout_id = db.Column(db.Integer, db.ForeignKey('workouts.id'), nullable=False)
+    exercise_id = db.Column(db.Integer, db.ForeignKey('exercises.id'), nullable=False)
+    reps = db.Column(db.Integer)
+    sets = db.Column(db.Integer)
+    duration_seconds = db.Column(db.Integer)
+
+    def __repr__(self):
+        return f'<WorkoutExercise {self.id}: workout={self.workout_id} exercise={self.exercise_id}>'
